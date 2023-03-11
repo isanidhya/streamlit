e2d1627268913411f9c8a0d7710f5c8cef3deb0a
isanidhya <115490812+isanidhya@users.noreply.github.com>
2023-03-11 13:35:07 +0530

    Add files via upload

---
+++ pages/3__About Us.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("About us")
+
+"""
+Samrat Ashok Technological Institute, a premier institute of the region was established by Late Maharaja Jiwaji Rao Scindia on November 1, 1960 with a donation from Gangajali Trust fund. The Institute started with B.E. in Civil Engineering, Mechanical Engineering & Electrical Engineering. Now the Institute offers 10 full time Under Graduate leading to degree in Bachelor of Engineering and 10 full time Post Graduate Courses ( 06 ME/M.Tech., MCA, MBA And M.Sc. in Applied Chemistry) in the areas of Engineering, Science and Management. It is an autonomous institute, which is Grant in Aid by Government of Madhya Pradesh and managed by the Maharaja Jiwaji Rao Education Society chaired by Hon'ble Shrimant Jyotiraditya M. Scindia. The college campus is spread over an area of 85 acres of lush green land with natural surroundings.
+"""
+st.header("Accreditation")
+
+"""
+Institute has been accredited by NAAC, New Delhi vide letter No. F-19-26/EC(SC-14)/DO/2016/39.1, New Delhi, Dated 29.03.2016 for Five years.
+"""
+
+st.header("Autonomy")
+st.header("RGPV")
+
+"""
+Institute has been accorded academic autonomy by Rajiv Gandhi Prodyogiki Vishwavidyalaya vide letter No. F-5/RGPV/Academic/Autonomy/2011/59, Bhopal, Dated 07.01.2011 w.e.f. session 2010-11.
+"""
+st.header("UGC")
+
+"""
+Institute has been accorded autonomy by University Grant Commission, New Delhi vide letter No. F. 22-1/2015(AC), New Delhi, Dated 01.05.2015 w.e.f. session 2015-16.
+"""
+st.header("Barkatullah University Bhopal")
+
+"""
+Institute has been accorded autonomy by Barkatullah University Bhopal vide letter No. 1618/Academic/Affiliation/2015,Bhopal, Dated 09.07.2015 up to 2020-21.
+
+
+Thus the Institute will attain the academic excellence in all fields and get many other benefits.
+
+"""
